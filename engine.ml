0b3654a0326c9a439bc275e8c159ffe4222e7b16
Łukasz Dąbek <sznurek@gmail.com>
2013-01-25 16:57:26 +0100

    Added continuity to the game, included come constants.

--- engine.ml
+++ engine.ml
@@ -1,7 +1,10 @@
 module Engine : sig
   val random_board : int * int -> float array array
-  val step : float array array -> float array array
-  val step_inplace : float array array ref -> float array array ref -> unit
+  val discrete_step : float array array -> float array array
+  val discrete_step_inplace : float array array ref -> float array array ref -> unit
+
+  val continuous_step : float -> float array array -> float array array
+  val continuous_step_inplace : float -> float array array ref -> float array array ref -> unit
 end = struct
   open Matrix
 
@@ -35,37 +38,52 @@ end = struct
     let s2 = masked_sum matrix c2 (x - r2, y - r2) in
       ((s2 -. s1) /. (a2 -. a1), s1 /. a1)
 
-  let alpha = 0.028
-  let (b1, b2) = (0.23075, 0.280)
-  let (d1, d2) = (0.280, 0.420)
-  let (r1, r2) = (2, 3)
+  let alpha = 0.08
+  let (b1, b2) = (0.240, 0.315) (* birth interval *)
+  let (d1, d2) = (0.210, 0.405) (* death interval *)
+  let (r1, r2) = (4, 12) (* inner and outer radius *)
   let sigma1 x a = 1.0 /. (1.0 +. exp (4.0 *. (a -. x) /. alpha))
   let sigma2 x a b = (sigma1 x a) *. (1.0 -. (sigma1 x b))
   let sigmam x y m = x *. (1.0 -. (sigma1 m 0.5)) +. y *. (sigma1 m 0.5)
   let transition n m = sigma2 n (sigmam b1 d1 m) (sigmam b2 d2 m)
 
-  let cell_step matrix (x, y) e = 
+  let discrete_cell_step matrix (x, y) e = 
     let (out_score, in_score) = score matrix x y r1 r2 in
-    if transition in_score out_score >= 0.5 then 1.0 else 0.0
+    transition in_score out_score
 
-  let step matrix = 
-    let (width, height) = Matrix.matrix_dim matrix in
-    let result = Array.make_matrix width height 0.0 in
-      Matrix.matrix_modify (cell_step matrix) result; result
+  let continuous_cell_step dt matrix (x, y) e = 
+    let scaled_score = 2.0 *. (discrete_cell_step matrix (x, y) e) -. 1.0 in
+    let score = scaled_score *. dt +. matrix.(x).(y) in
+    if score < 0.0 then 0.0
+    else if score > 1.0 then 1.0
+    else score
 
   let swapref a b = 
     let tmp = !a in
       a := !b;
       b := tmp
 
-  let step_inplace matrix buffer = 
-    let (width, height) = Matrix.matrix_dim !matrix in
-      Matrix.matrix_modify (cell_step !matrix) !buffer; 
-      swapref matrix buffer
+  let discrete_step matrix = 
+    let (width, height) = Matrix.matrix_dim matrix in
+    let result = Array.make_matrix width height 0.0 in
+      Matrix.matrix_modify (discrete_cell_step matrix) result; result
+
+  let discrete_step_inplace matrix buffer = 
+    Matrix.matrix_modify (discrete_cell_step !matrix) !buffer; 
+    swapref matrix buffer
+
+  let continuous_step dt matrix = 
+    let (width, height) = Matrix.matrix_dim matrix in
+    let result = Array.make_matrix width height 0.0 in
+      Matrix.matrix_modify (continuous_cell_step dt matrix) result; result
+
+  let continuous_step_inplace dt matrix buffer = 
+    Matrix.matrix_modify (continuous_cell_step dt !matrix) !buffer;
+    swapref matrix buffer
 
   let random_board (width, height) = 
     let matrix = Array.make_matrix width height 0.0 in 
-      Matrix.matrix_modify (fun _ _ -> if Random.bool() then 1.0 else 0.0) matrix;
+      Matrix.matrix_modify (fun _ _ -> Random.float 1.0) matrix;
       matrix
 end
 
--- main.ml
+++ main.ml
@@ -1,28 +1,28 @@
-open Unix;;
 open Graphics;;
-open Printf;;
 open Matrix;;
 open Engine;;
 
-let (width, height) = (150, 150)
-let scale = 3
+let (width, height) = (100, 100)
+let scale = 2
 
-let rec draw_steps board buffer n = 
-  if n = 0 then sleep 1000 else
-    (print_string "drawing"; print_newline ();
-    set_color (rgb 0 0 0);
-    fill_rect 0 0 (width * scale) (height * scale);
-    set_color (rgb 255 255 255);
-    Matrix.matrix_iteri (fun (x, y) e -> if e > 0.0 then fill_rect (x * scale) (y * scale) scale scale) !board;
-    Engine.step_inplace board buffer;
-    draw_steps board buffer (n - 1))
+let set_cell_color cell = 
+  let value = int_of_float (cell *. 255.0)
+  in set_color (rgb value value value)
+
+let rec draw_steps board buffer = 
+  set_color (rgb 0 0 0);
+  fill_rect 0 0 (width * scale) (height * scale);
+  set_color (rgb 255 255 255);
+  Matrix.matrix_iteri (fun (x, y) e -> set_cell_color e; fill_rect (x * scale) (y * scale) scale scale) !board;
+  Engine.continuous_step_inplace 0.3 board buffer;
+  draw_steps board buffer
 
 let main () = 
   Random.self_init ();
   let board = Engine.random_board (width, height) in
   let buffer = Array.make_matrix width height 0.0 in
   open_graph (" " ^ (string_of_int (width * scale)) ^ "x" ^ (string_of_int (height * scale)));
-  draw_steps (ref board) (ref buffer) 400
+  draw_steps (ref board) (ref buffer)
 ;;
 
 main();;
